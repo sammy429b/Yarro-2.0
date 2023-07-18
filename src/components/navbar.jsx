124ea280be62189429dc4669399bdea5e7ec402c
samarth bahirgonde <samarthnbahirgonde49@gmail.com>
2023-07-19 00:09:37 +0530

    modification in Index page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,47 +1,46 @@
-export default function Navbar({link_name,link_icon}) {
+export default function Navbar({ link_name, link_icon }) {
     return (
-    <header class="w-full flex flex-row items-center place-content-between">
-    <a href="/" class="pl-8 text-2xl font-medium mt-4 group-data-[checked=true]:text-white">
-        Yarro
-    </a>
-    <div class="navTabs flex gap-x-8">
+        <header className="w-full h-[5rem] flex flex-row items-center content-between justify-between px-8">
+            <a href="/" className="h-[5rem]  text-2xl items-center flex font-medium group-data-[checked=true]:text-white">
+                Yarro
+            </a>
 
-        {/* {% for link in nav %} */}
-        <a href="{{link.link}}" class="hover:cursor-pointer group-data-[checked=true]:text-white">
-            <p class="material-symbols-outlined pt-4 mx-2 text-2xl select-none" id="homebtn">{link_icon}</p>
-            <p class="select-none">{link_name}</p>
-        </a>
-        {/* {% endfor %} */}
-    </div>
-
-
-    <div class="items-center flex">
-        <label for="theme">
-            <span id="theme_button"
-                class="material-symbols-outlined pt-4 px-4 text-4xl group-data-[checked=true]:text-white select-none hover:cursor-pointer">
-                dark_mode
-            </span>
-        </label>
-        <input type="checkbox" id="theme" class="hidden"/>
-        {/* {% if nav %} */}
-        <div class="relative inline-block group/nav ">
-            <div class="profile-container pt-4 pr-8 min-w-[50px] max-w-[50px] min-h-[50px] mr-4">
-                <img id="profile-img" src="" alt="pfp" class="min-w-[50px] h-[50px] rounded-full select-none" />
+            <div className="navTabs  h-[5rem] flex gap-x-8">
+                <a href="{{link.link}}" className="hover:cursor-pointer group-data-[checked=true]:text-white">
+                    <p className="material-symbols-outlined text-2xl select-none" id="homebtn">{link_icon}</p>
+                    <p className="select-none">{link_name}</p>
+                </a>
             </div>
-            <div
-                class="navs group-hover/nav:block absolute hidden w-36 right-1 z-1 shadow-xl group-data-[checked=true]:text-white">
-                <p class="py-2 hover:cursor-pointer pl-2 bg-white hover:bg-gray-300 group-data-[checked=true]:bg-gray-900 group-data-[checked=true]:hover:bg-black"
-                    id="profile-btn">
-                    Profile
-                </p>
-                <p class="py-2 hover:cursor-pointer pl-2 bg-white hover:bg-gray-300 group-data-[checked=true]:bg-gray-900 group-data-[checked=true]:hover:bg-black"
-                    id="logout-btn">
-                    Logout
-                </p>
+
+
+            <div className=" h-[4rem] pt-4 items-center flex">
+                <label>
+                    <span id="theme_button"
+                        className="material-symbols-outlined px-6 text-4xl group-data-[checked=true]:text-white select-none hover:cursor-pointer">
+                        dark_mode
+                    </span>
+                </label>
+
+                <input type="checkbox" id="theme" className="hidden" />
+
+                <div className="relative inline-block group/nav ">
+                    <div className="profile-container pt-4 pr-8 min-w-[50px] max-w-[50px] min-h-[50px] mr-4">
+                        <img id="profile-img" src="" alt="pfp" className="min-w-[50px] h-[50px] rounded-full select-none" />
+                    </div>
+                    <div
+                        className="navs group-hover/nav:block absolute hidden w-36 right-1 z-1 shadow-xl group-data-[checked=true]:text-white">
+                        <p className="py-2 hover:cursor-pointer pl-2 bg-white hover:bg-gray-300 group-data-[checked=true]:bg-gray-900 group-data-[checked=true]:hover:bg-black"
+                            id="profile-btn">
+                            Profile
+                        </p>
+                        <p className="py-2 hover:cursor-pointer pl-2 bg-white hover:bg-gray-300 group-data-[checked=true]:bg-gray-900 group-data-[checked=true]:hover:bg-black"
+                            id="logout-btn">
+                            Logout
+                        </p>
+                    </div>
+                </div>
+                {/* {% endif %} */}
             </div>
-        </div>
-        {/* {% endif %} */}
-    </div>
-</header>
-)
+        </header>
+    )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,52 +1,87 @@
 const Index = () => {
   return (
     <>
-     <main className="mt-28">
+      <main className="mt-2 h-[40.5rem] lg:w-full lg:h-[41rem] bg-[#FAFAFA]">
+        <div
+          id="main-container"
+          className="w-full lg:h-[40rem] flex justify-center lg:place-content-evenly"
+        >
+          <div
+            id="image-container"
+            className="lg:w-1/2 lg:h-[40rem] lg:flex lg:items-center lg:justify-center hidden className="
+          >
+            <img
+              className="w-[30rem]"
+              src="images/landing_image.svg"
+              alt="someimage"
+            />
+          </div>
 
-<div className="containerx flex justify-center lg:place-content-evenly lg:mr-10 px-16">
-    <div className="image-container hidden lg:block">
-        <img className="w-md max-w-xl" src="images/login.png" alt="someimage" />
-    </div>
-
-    <div
-        className="form-container border rounded-[3rem] flex flex-col px-8 py-[70px] border-white bg-white group-data-[checked=true]:bg-black group-data-[checked=true]:border-gray-900">
-        <form id="login_form"
-            className="login-form flex flex-col w-[370px] lg:w-[400px] transition-[width] duration-500 group-data-[checked=true]:bg-black">
-            <div className="phrase-container flex justify-center">
-                <p className="phrase text-3xl font-semibold pb-8 text-center group-data-[checked=true]:text-white">
-                    Login to access your account
+          <div
+            id="form-container"
+            className="className= lg:w-1/2 lg:h-[40rem] rounded-[3rem] flex items-center justify-center px-8 py-[70px] border-black group-data-[checked=true]:bg-black group-data-[checked=true]:border-gray-900"
+          >
+            <form
+              id="login_form"
+              className=" bg-white rounded-[1.5rem] flex gap-y-2 flex-col w-[30rem] lg:w-[32rem] lg:h-[30rem] p-6 transition-[width] duration-500 group-data-[checked=true]:bg-black"
+            >
+              <div className="phrase-container flex justify-center">
+                <p className="phrase text-3xl font-semibold text-center py-4 group-data-[checked=true]:text-white">
+                  Login to access your account
                 </p>
-            </div>
+              </div>
 
-            <input
-                className="m-[8px] rounded-lg h-12 pl-3 border-2 group-data-[checked=true]:bg-[#3b4148] group-data-[checked=true]:text-white"
-                type="text" name="uname" placeholder="username" id="uname" />
-            <div id="pass"
-                className="outline outline-0 m-[8px] rounded-lg h-12 pl-3 pr-2 border-2 flex justify-between items-center group-data-[checked=true]:bg-[#3b4148]">
+              <input
+                className="m-[8px] border-2 rounded-lg h-12 pl-3 className= group-data-[checked=true]:bg-[#3b4148] group-data-[checked=true]:text-white outline-none"
+                type="text"
+                name="uname"
+                placeholder="username"
+                id="uname"
+              />
+              <div
+                id="pass"
+                className="outline-none border-2 m-[8px] rounded-lg h-12 pl-3 pr-2 className= flex justify-between items-center group-data-[checked=true]:bg-[#3b4148]"
+              >
                 <input
-                    className="h-10 border-none w-full outline-none group-data-[checked=true]:text-white group-data-[checked=true]:bg-[#3b4148]"
-                    type="password" name="passwd" placeholder="Password" id="passwd" />
+                  className="h-10 border-none w-full outline-none group-data-[checked=true]:text-white group-data-[checked=true]:bg-[#3b4148]"
+                  type="password"
+                  name="passwd"
+                  placeholder="Password"
+                  id="passwd"
+                />
                 <input type="checkbox" id="pw" className="hidden" />
-                <label  id="vis" className="material-symbols-outlined select-none hover:cursor-pointer">
-                    visibility
+                <label
+                  id="vis"
+                  className="material-symbols-outlined select-none hover:cursor-pointer"
+                >
+                  visibility
                 </label>
-            </div>
-            <a href="/password/reset"
-                className="forgot-text w-full flex text-sm px-4 justify-end hover:underline underline-offset-1 group-data-[checked=true]:text-white">
+              </div>
+              <a
+                href="/password/reset"
+                className="forgot-text w-full flex text-sm px-4 justify-end hover:underline underline-offset-1 group-data-[checked=true]:text-white"
+              >
                 Forgot password
-            </a>
-            <input className="mt-[20px] rounded-full h-[40px] text-white text-m bg-[#7034E4] hover:cursor-pointer "
-                type="submit" name="login_btn" id="login_btn" value="Login" />
-            <a href="/register"
-                className="register-text flex justify-center mt-[50px] hover:underline underline-offset-1 group-data-[checked=true]:text-white">
+              </a>
+              <input
+                className="mt-[20px] rounded-full h-[40px] text-white text-m bg-blue-700 hover:bg-blue-600 duration-200 hover:cursor-pointer "
+                type="submit"
+                name="login_btn"
+                id="login_btn"
+                value="Login"
+              />
+              <a
+                href="/register"
+                className="register-text flex justify-center mt-[3rem] hover:underline underline-offset-1 group-data-[checked=true]:text-white"
+              >
                 Sign up instead?
-            </a>
-        </form>
-    </div>
-</div>
-</main> 
+              </a>
+            </form>
+          </div>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
