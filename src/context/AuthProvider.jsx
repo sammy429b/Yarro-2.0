c394195ac6cc9f4ae28090e5a298ab88c5ca9bf4
Ayush Deshpande <93706892+asyncdoggo@users.noreply.github.com>
2023-07-21 15:53:25 +0000

    Fix issuse from last merge

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,27 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({ login: false, uid: '', uname: '' });
 
 export function AuthProvider({ children }) {
     const [auth, setAuth] = useState({ login: false, uid: '', uname: '' });
-    try {
-        const response = fetch('http://localhost:3000/api/login', {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then((response) => response.json())
-
-        const status = response.status;
-        // uid and uname
-        if (status === "success")
-            setAuth({ login: true });
-    }
-    catch (err) {
-        console.log(err);
-    }
+    
+    useEffect(() => {
+        try {
+            const response = fetch('http://localhost:3000/api/login', {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }).then((response) => response.json())
+    
+            const status = response.status;
+            // uid and uname
+            if (status === "success")
+                setAuth({ login: true });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },[])
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,6 +2,73 @@ import { useEffect, useContext } from "react";
 import AuthContext from "../context/AuthProvider";
 import { useSnackbar } from "notistack";
 const Index = () => {
+  
+  const { setAuth } = useContext(AuthContext);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const LoginForm = async function (e) {
+      e.preventDefault();
+      const form = new FormData(e.target);
+      const data = Object.fromEntries(form.entries());
+      console.log(data)
+      try {
+          const response = await fetch("http://localhost:3000/api/login", {
+              method: 'POST',
+              mode: 'cors',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                  "Authorization": "Basic " + btoa(data["uname"] + ":" + data["passwd"])
+              }
+          });
+
+          const responseData = await response.json();
+
+          const status = responseData.status;
+          console.log(responseData)
+          if (status === "success") {
+              setAuth({ login: true, uid: responseData.uid, uname: responseData.uname });
+              window.location.reload()
+          }
+          else {
+              console.log(status)
+              enqueueSnackbar(status, {
+                  variant: 'error',
+                  sx: {
+                      "& .SnackbarContent-root": {
+                          width: 100,
+                          fontSize: 16
+                      }
+                  }
+              })
+          }
+      }
+      catch (err) {
+          console.log(err);
+      }
+  }
+
+  useEffect(() => {
+      document.getElementById("passwd").addEventListener("focusin", (e) => {
+          document.getElementById("pass").style.outlineWidth = "2px"
+      })
+
+      document.getElementById("passwd").addEventListener("focusout", (e) => {
+          document.getElementById("pass").style.outlineWidth = "0px"
+      })
+
+      document.getElementById("vis").addEventListener("click", (e) => {
+          if (document.getElementById("vis").innerText === 'visibility') {
+              document.getElementById("vis").innerHTML = "visibility_off"
+              document.getElementById("passwd").type = "text"
+          }
+          else {
+              document.getElementById("vis").innerHTML = "visibility"
+              document.getElementById("passwd").type = "password"
+          }
+      })
+
+  }, [])
   return (
     <>
       <main className="mt-2 h-[40.5rem] lg:w-full lg:h-[41rem] bg-[#FAFAFA]">
@@ -87,120 +154,6 @@ const Index = () => {
   );
 };
 
-    const { setAuth } = useContext(AuthContext);
-    const { enqueueSnackbar } = useSnackbar();
 
-    const LoginForm = async function (e) {
-        e.preventDefault();
-        const form = new FormData(e.target);
-        const data = Object.fromEntries(form.entries());
-        console.log(data)
-        try {
-            const response = await fetch("http://localhost:3000/api/login", {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Authorization": "Basic " + btoa(data["uname"] + ":" + data["passwd"])
-                }
-            });
-
-            const responseData = await response.json();
-
-            const status = responseData.status;
-            console.log(responseData)
-            if (status === "success") {
-                setAuth({ login: true, uid: responseData.uid, uname: responseData.uname });
-                window.location.reload()
-            }
-            else {
-                console.log(status)
-                enqueueSnackbar(status, {
-                    variant: 'error',
-                    sx: {
-                        "& .SnackbarContent-root": {
-                            width: 100,
-                            fontSize: 16
-                        }
-                    }
-                })
-            }
-        }
-        catch (err) {
-            console.log(err);
-        }
-    }
-
-    useEffect(() => {
-        document.getElementById("passwd").addEventListener("focusin", (e) => {
-            document.getElementById("pass").style.outlineWidth = "2px"
-        })
-
-        document.getElementById("passwd").addEventListener("focusout", (e) => {
-            document.getElementById("pass").style.outlineWidth = "0px"
-        })
-
-        document.getElementById("vis").addEventListener("click", (e) => {
-            if (document.getElementById("vis").innerText === 'visibility') {
-                document.getElementById("vis").innerHTML = "visibility_off"
-                document.getElementById("passwd").type = "text"
-            }
-            else {
-                document.getElementById("vis").innerHTML = "visibility"
-                document.getElementById("passwd").type = "password"
-            }
-        })
-
-    }, [])
-
-    return (
-        <>
-            <main className="mt-28">
-
-                <div className="containerx flex justify-center lg:place-content-evenly lg:mr-10 px-16">
-                    <div className="image-container hidden lg:block">
-                        <img className="w-md max-w-xl" src="images/login.png" alt="someimage" />
-                    </div>
-
-                    <div
-                        className="form-container border rounded-[3rem] flex flex-col px-8 py-[70px] border-white bg-white group-data-[checked=true]:bg-black group-data-[checked=true]:border-gray-900">
-                        <form id="login_form" onSubmit={LoginForm}
-                            className="login-form flex flex-col w-[370px] lg:w-[400px] transition-[width] duration-500 group-data-[checked=true]:bg-black">
-                            <div className="phrase-container flex justify-center">
-                                <p className="phrase text-3xl font-semibold pb-8 text-center group-data-[checked=true]:text-white">
-                                    Login to access your account
-                                </p>
-                            </div>
-
-                            <input
-                                className="m-[8px] rounded-lg h-12 pl-3 border-2 group-data-[checked=true]:bg-[#3b4148] group-data-[checked=true]:text-white"
-                                type="text" name="uname" placeholder="username" id="uname" />
-                            <div id="pass"
-                                className="outline outline-0 m-[8px] rounded-lg h-12 pl-3 pr-2 border-2 flex justify-between items-center group-data-[checked=true]:bg-[#3b4148]">
-                                <input
-                                    className="h-10 border-none w-full outline-none group-data-[checked=true]:text-white group-data-[checked=true]:bg-[#3b4148]"
-                                    type="password" name="passwd" placeholder="Password" id="passwd" />
-                                <label id="vis" className="material-symbols-outlined select-none hover:cursor-pointer">
-                                    visibility
-                                </label>
-                            </div>
-                            <a href="/password/reset"
-                                className="forgot-text w-full flex text-sm px-4 justify-end hover:underline underline-offset-1 group-data-[checked=true]:text-white">
-                                Forgot password
-                            </a>
-                            <input className="mt-[20px] rounded-full h-[40px] text-white text-m bg-[#7034E4] hover:cursor-pointer "
-                                type="submit" name="login_btn" id="login_btn" value="Login" />
-                            <a href="/register"
-                                className="register-text flex justify-center mt-[50px] hover:underline underline-offset-1 group-data-[checked=true]:text-white">
-                                Sign up instead?
-                            </a>
-                        </form>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
-}
 
 export default Index;
