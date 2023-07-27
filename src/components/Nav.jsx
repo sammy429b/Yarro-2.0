3414441ab32edea0fe2e18a0f8d1ca7eb7e35830
samarth bahirgonde <samarthnbahirgonde49@gmail.com>
2023-07-27 15:50:24 +0530

    adding new nindex page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
       <div id="Nav" className="w-full bg-[#EDF4F6] h-[5rem] text-black flex items-center justify-between">
         <div id="Logo" className=" flex gap-x-1 items-center ml-4 text-2xl font-medium font-sans">
             <img src="images/logo.png" alt="" className="w-[2rem]"/>
-            <h2 className="">Yarro</h2>
+            <h1 className="">Yarro</h1>
         </div>
         <div id="Nav-btns" className="flex justify-center gap-x-4 items-center  w-[16rem] h-[4rem] ">
             <button className="w-[10rem] h-[2.6rem] bg-purple-600 text-white rounded px-2">Log In</button>
--- src/pages/NewIndex.jsx
+++ src/pages/NewIndex.jsx
@@ -1,9 +1,29 @@
-
+import {Typewriter} from 'react-simple-typewriter'
 
 const NewIndex = () => {
   return (
     <>
-      <div id="mainConatiner" className=""></div>
+      <div id="mainConatiner" className="flex flex-col-reverse lg:flex-row">
+        <div id="leftContainer" className='flex flex-col justify-center pl-8 gap-y-3 w-full h-[40.6rem] lg:w-1/2 '>
+          <h2 className='flex gap-x-2 w-[82%] lg:w-full text-[2rem] lg:text-5xl sm:text-4xl font-semibold'>
+            <p className='text-blue-400'>Make connections </p>
+            <Typewriter  words={[" with Friends", " with People"]} loop={false} cursor
+            cursorStyle='!'
+            typeSpeed={100}
+            deleteSpeed={100}
+            delaySpeed={2000}/>
+          </h2>
+        <p className='text-gray-00 text-xl sm:text-2xl lg:text-3xl w-[80%] lg:w-full pr-4'>
+          The Simple way to connect and look back on moments forever.
+        </p>
+
+        <button className='mt-2 w-[8rem] h-[3rem] rounded text-white bg-purple-600'>Get Started</button>
+        </div>
+
+        <div id='rightConatiner' className='w-full lg:w-1/2 h-[40.6rem]'>
+          <img src="/images/connect.jpg" alt=""  className='mix-blend-multiply'/>
+        </div>
+      </div>
     </>
   )
 }
