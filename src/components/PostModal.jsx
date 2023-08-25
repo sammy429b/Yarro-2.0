25c9a48cbafc01d1ecb3f23214e7aa973feb4b3d
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-08-25 19:42:42 +0530

    Updating postmodal component

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -2,20 +2,20 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
+  content: {
+    top: '40%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 
 Modal.setAppElement('#root');
 
-export default function PostModal({open,setOpen}) {
-    let subtitle;
+export default function PostModal({ open, setOpen }) {
+  let subtitle;
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -26,20 +26,28 @@ export default function PostModal({open,setOpen}) {
   }
 
   return (
-      <Modal
-       isOpen={open}
-       onAfterOpen={afterOpenModal}
-       onRequestClose={handleClose}
-       style={customStyles}
-       contentLabel="New Post"
-      >
-        <div className="w-[400px] flex flex-col items-center">
-        <p ref={(_subtitle) => (subtitle = _subtitle)} className='w-64 text-xl text-center text-bold'>Create new post</p>
-
-
-
-        <button onClick={handleClose}>close</button>
+    <Modal
+      isOpen={open}
+      onAfterOpen={afterOpenModal}
+      onRequestClose={handleClose}
+      style={customStyles}
+      contentLabel="New Post"
+    >
+      <div className="w-[600px] h-[20rem] flex flex-col items-center">
+        <div className='w-full  flex items-center justify-between'>
+          <p ref={(_subtitle) => (subtitle = _subtitle)} className='w text-xl text-center text-bold'>Create new post</p>
+          <button onClick={handleClose} className='font-semibold text-xl'>
+            <i className='fa fa-close'></i>
+          </button>
+        </div>
+        <div className='w-full h-[15rem] mt-2'>
+          <textarea className=" w-full text-xl h-[14rem] outline-none p-2 resize-none" autoFocus rows={10} type="text" name="" id="" placeholder='Share your thoughts' wrap="soft" />
+        </div>
+        <div className='w-full flex justify-end items-center gap-x-4'>
+          <button className='text-violet-600 bg-white w-[7rem] h-[2.3rem] rounded-[3rem] border-violet-600 border'>cancel</button>
+          <button className='bg-violet-600 text-white w-[7rem] h-[2.3rem] rounded-[3rem]'>Send</button>
+        </div>
       </div>
-      </Modal>
+    </Modal>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,8 +9,8 @@ import BasicModal from "../components/PostModal"
 
 const Main = () => {
 
-    const [posts,setPosts] = useState([])
-    const {auth} = useContext(AuthContext)
+    const [posts, setPosts] = useState([])
+    const { auth } = useContext(AuthContext)
 
     const [open, setOpen] = useState(false);
 
@@ -21,7 +21,7 @@ const Main = () => {
         const response = await fetch(`${import.meta.env.VITE_SERVER}/api/posts?page=0`, {
             method: "GET",
             mode: "cors",
-            credentials:"include",
+            credentials: "include",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
@@ -29,17 +29,17 @@ const Main = () => {
         });
 
         const data = await response.json()
-        if(data.status == "success"){
+        if (data.status == "success") {
             setPosts(data.data.reverse())
         }
     }
 
 
-    const likePost = async (pid,islike) => {
+    const likePost = async (pid, islike) => {
         const response = await fetch(`${import.meta.env.VITE_SERVER}/api/like`, {
             method: "POST",
             mode: "cors",
-            credentials:"include",
+            credentials: "include",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
@@ -66,17 +66,17 @@ const Main = () => {
 
     useEffect(() => {
         getPosts()
-    },[])
+    }, [])
 
 
 
     return (
         <>
             <main className="bg-white dark:bg-gray-900 h-[100%]">
-                <BasicModal open={open} setOpen={setOpen}/>
+                <BasicModal open={open} setOpen={setOpen} />
                 <div id="createPostContainer" className="w-full h-[8rem] py-4 flex justify-center items-center bg-white dark:bg-gray-900 ">
                     <div id="wrapPost" className="bg-[#F6F8FA] w-full mt-4 p-4 sm:w-[50%] dark:bg-gray-800">
-                        <input  onClick={() => {setOpen(true)}} type="text" className="w-full rounded my-2 p-4 h-[3rem] bg-[#F6F8FA] disabled dark:bg-gray-700" placeholder="Share your thougths" />
+                        <input onClick={() => { setOpen(true) }} type="text" className="outline-none w-full rounded my-2 p-4 h-[3rem] bg-[#F6F8FA] disabled dark:bg-gray-700" placeholder="Share your thougths" />
                         <div id="postBtn" className="w-full flex justify-between pl-2 pr-4">
                             <div className="text-black ">
                                 <i className="fa-solid fa-images text-2xl text-black dark:text-white"></i>
@@ -91,40 +91,40 @@ const Main = () => {
                 <div id="mainFeed" className="w-full pt-8 h-auto flex flex-col items-center justify-center">
 
 
-                    {posts.map((post) => 
+                    {posts.map((post) =>
                         <div id="wrapFeed" key={post.post_id} className="w-full sm:w-[50%] flex justify-center bg-[#F6F8FA] rounded-lg py-2 mt-4 dark:bg-gray-800 text-black dark:text-white">
-                        <div id="feed1 " className="flex flex-col sm:w-[98%] pr-4">
-                            <div id="userDetails" className="ml-2 flex justify-between m">
-                                <div id="userImage" className="flex">
-                                    <img src={`${import.meta.env.VITE_SERVER}/image/${post.uid}`} alt="" className="w-[4rem]" />
-                                    <div className="ml-2">
-                                        <p className="font-semibold">{post.fullname}</p>
-                                        <p>@{post.uname}</p>
+                            <div id="feed1 " className="flex flex-col sm:w-[98%] pr-4">
+                                <div id="userDetails" className="ml-2 flex justify-between m">
+                                    <div id="userImage" className="flex">
+                                        <img src={`${import.meta.env.VITE_SERVER}/image/${post.uid}`} alt="" className="w-[4rem]" />
+                                        <div className="ml-2">
+                                            <p className="font-semibold">{post.fullname}</p>
+                                            <p>@{post.uname}</p>
+                                        </div>
+                                    </div>
+                                    <div className=" mr-2">
+                                        {post.datetime}
                                     </div>
                                 </div>
-                                <div className=" mr-2">
-                                    {post.datetime}
-                                </div>
-                            </div>
-                            <div id="postContent" className="ml-20 text-justify ">
-                                <p>{
-                                    post.content_type=="text" ? post.content : <img src={`${import.meta.env.VITE_SERVER}/post/images/${post.uid}`}/>
+                                <div id="postContent" className="ml-20 text-justify ">
+                                    <p>{
+                                        post.content_type == "text" ? post.content : <img src={`${import.meta.env.VITE_SERVER}/post/images/${post.uid}`} />
                                     }</p>
 
-                                <div id="likeComment" className="w-full h-[3rem] px-4 my-1 items-center flex gap-x-6 text-[3rem]">
-                                    <div id="likeCount" className=" flex gap-2">
-                                        <img onClick={() => {likePost(post.post_id,1)}} className="w-[26px] hover:cursor-pointer dark:filter dark:invert-[100%]" src={post.islike? thumb_up : thumb_up_off}/>
-                                        <span className="text-base">{post.lc}</span>
-                                    </div>
-                                    <div className="flex gap-2">
-                                        <img onClick={() => {likePost(post.post_id,0)}} className="w-[26px] hover:cursor-pointer dark:filter dark:invert-[100%]" src={post.isdislike? thumb_down : thumb_down_off}/>
-                                        <span className="text-base">{post.dlc}</span>
-                                    </div>
+                                    <div id="likeComment" className="w-full h-[3rem] px-4 my-1 items-center flex gap-x-6 text-[3rem]">
+                                        <div id="likeCount" className=" flex gap-2">
+                                            <img onClick={() => { likePost(post.post_id, 1) }} className="w-[26px] hover:cursor-pointer dark:filter dark:invert-[100%]" src={post.islike ? thumb_up : thumb_up_off} />
+                                            <span className="text-base">{post.lc}</span>
+                                        </div>
+                                        <div className="flex gap-2">
+                                            <img onClick={() => { likePost(post.post_id, 0) }} className="w-[26px] hover:cursor-pointer dark:filter dark:invert-[100%]" src={post.isdislike ? thumb_down : thumb_down_off} />
+                                            <span className="text-base">{post.dlc}</span>
+                                        </div>
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     )}
                 </div>
             </main>
